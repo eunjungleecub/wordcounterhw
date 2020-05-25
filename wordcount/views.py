13fa45f74fa2cc7d00643230821682416b019449
eunjungleo <asaprocky123@likelion.org>
2020-05-25 16:39:15 +0900

    rm comments

--- wordcount/views.py
+++ wordcount/views.py
@@ -13,10 +13,10 @@ def count(request):
     word_dictionary = {}
     for word in word_list:
         if word in word_dictionary:
-            # Increase
+
             word_dictionary[word] += 1
         else:
-            # add to the dictionary
+
             word_dictionary[word] = 1
 
     return render(request, 'count.html', {'fulltext': full_text, 'total': len(word_list), 'dictionary': word_dictionary.items()})
